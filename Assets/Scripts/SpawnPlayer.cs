6d8635db809c94e703f3c624f1ac5896dc04b779
Daniel Mota <Daniel Mota>
2016-06-06 15:24:37 +0100

    created the player prefab added empty gameobjects to serve as diferent spawn points created SpawnPlayer.cs that spawns the player everytime in a random fation solves #9

---
+++ Assets/Scripts/SpawnPlayer.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class SpawnPlayer : MonoBehaviour
+{
+
+
+	public bool respawn = false;
+	private GameObject[] playerSpawnPoints;
+	private GameObject player;
+	// Use this for initialization
+	void Start ()
+	{
+		playerSpawnPoints = GameObject.FindGameObjectsWithTag ("PlayerSpawn");
+		player = GameObject.FindGameObjectWithTag ("Player");
+		ReSpawnPlayer ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (respawn) {
+			ReSpawnPlayer ();
+			respawn = false;
+		}
+	}
+
+	private void ReSpawnPlayer ()
+	{
+		if (player)
+			Destroy (player);
+		
+	
+		InstantiatePlayer ();
+
+	}
+
+	private void InstantiatePlayer ()
+	{
+		int point = UnityEngine.Random.Range (0, playerSpawnPoints.Length);
+		print (point);
+		player = Instantiate (Resources.Load ("Prefabs/Player"))as GameObject;
+		player.transform.position = playerSpawnPoints [point].transform.position;
+		player.transform.rotation = playerSpawnPoints [point].transform.rotation;
+		player.name = "Player";
+
+	}
+}
