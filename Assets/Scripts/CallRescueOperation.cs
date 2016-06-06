b350a8617ff2d23398684a2868e85d74814677c6
Daniel Mota <Daniel Mota>
2016-06-06 18:52:44 +0100

    added audio clip and implementation fro callig the helicopter for rescue resolves #10

---
+++ Assets/Scripts/CallRescueOperation.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CallRescueOperation : MonoBehaviour
+{
+	AudioClip heliCall;
+	GameObject heli;
+
+	void Start ()
+	{
+		heli = GameObject.Find ("Helicopter");
+		heliCall = Resources.Load ("Audio/heli_call") as AudioClip;
+	}
+	// Update is called once per frame
+	void Update ()
+	{
+
+		if (Input.GetButton ("CallHeli")) {
+			CallRescue ();
+		}
+	
+	
+	}
+
+	void CallRescue ()
+	{
+		
+		AudioSource.PlayClipAtPoint (heliCall, transform.position);
+
+	
+	}
+}
