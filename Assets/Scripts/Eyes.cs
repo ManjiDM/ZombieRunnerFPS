801226f672cdbd39aed11bd082e842c0018a3b6a
Daniel Mota <Daniel Mota>
2016-06-06 18:51:41 +0100

    added zoomed boolean to stop the zooming from being infinite as long as key was pressed refs #10

--- Assets/Scripts/Eyes.cs
+++ Assets/Scripts/Eyes.cs
@@ -6,6 +6,7 @@ public class Eyes : MonoBehaviour
 
 	private Camera eyes;
 	private float defaultFOV;
+	private bool zoomed = false;
 	// Use this for initialization
 	void Start ()
 	{
@@ -16,10 +17,13 @@ public class Eyes : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-		if (Input.GetButton ("Zoom")) {
+		
+		if (Input.GetButtonDown ("Zoom") && !zoomed) {
+			
 			eyes.fieldOfView /= 3f;
-		} else {
+		} else if (Input.GetButtonUp ("Zoom")) {
 			eyes.fieldOfView = defaultFOV;
+			zoomed = false;
 		}
 
 	
