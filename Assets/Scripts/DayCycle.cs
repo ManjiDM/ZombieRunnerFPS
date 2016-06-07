f5ad476df203feda0eb555d0285b3b88e0a7ae82
Daniel Mota <Daniel Mota>
2016-06-07 15:16:04 +0100

    added procedural skybox and simple day/night cycle resolves #14

---
+++ Assets/Scripts/DayCycle.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class DayCycle : MonoBehaviour
+{
+
+	[Tooltip ("each second of real life represent minutes in game if value 60")]
+	public float minutesPerSecond = 60;
+	private Quaternion startRotation;
+	// Use this for initialization
+	void Start ()
+	{
+		startRotation = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float angle = Time.deltaTime / 360 * minutesPerSecond;
+		transform.RotateAround (transform.position, Vector3.forward, angle);
+	}
+}
