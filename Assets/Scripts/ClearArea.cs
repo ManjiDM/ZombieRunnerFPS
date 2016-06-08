00df4054293b526135f00e4e47e8728e7f3861fe
Daniel Mota <Daniel Mota>
2016-06-08 22:24:38 +0100

    Resolves #15 Found clear area to call for helicopter

---
+++ Assets/Scripts/ClearArea.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClearArea : MonoBehaviour
+{
+	AudioClip clearArea;
+	float clearTime	= 0f;
+	bool areaFound = false;
+	// Use this for initialization
+	void Start ()
+	{
+		clearArea = Resources.Load ("Audio/clear_area") as AudioClip;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		clearTime += Time.deltaTime;
+		if (clearTime > 2f && !areaFound) {
+			areaFound = !areaFound;
+			AudioSource.PlayClipAtPoint (clearArea, transform.parent.position);
+		}
+			
+	}
+
+	void OnTriggerStay ()
+	{
+		clearTime = 0f;
+	}
+}
