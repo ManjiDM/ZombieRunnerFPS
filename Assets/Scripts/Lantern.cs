6b0083358d31c669f5484c0eb5ab7bb53a1cc51a
Daniel Mota <Daniel Mota>
2016-06-09 09:24:38 +0100

    Resolves #25 created a spotlight facing the front of the Player  added it to the player prefab  created lantern.cs script that allows (L) to enable and disable the light source

---
+++ Assets/Scripts/Lantern.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Lantern : MonoBehaviour
+{
+
+	private Light lantern;
+	// Use this for initialization
+	void Start ()
+	{
+		lantern = GetComponent <Light> ();
+		lantern.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.L)) {
+			ToggleLight ();
+		}
+	}
+
+	void ToggleLight ()
+	{
+		lantern.enabled = !lantern.enabled;
+	}
+}
